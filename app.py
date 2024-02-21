23b46851ff433bedf3d00babfc7b0b1269efab06
Suman Paudel <dastonsuman1997@gmail.com>
2024-02-21 21:22:55 +0545

    gemini pro in action

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-genai.configure(os.getenv('GOOGLE_API_KEY'))
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 model = genai.GenerativeModel('gemini-pro')
 
 # load gemini to answer question
@@ -16,3 +15,18 @@ model = genai.GenerativeModel('gemini-pro')
 def get_gemini(question: str):
     response = model.generate_content(question)
     return response.text
+
+
+st.set_page_config(page_title='Suman"s Gemini Q&A Bot')
+
+st.header('Gemini Pro in Action')
+
+input_qa = st.text_input('Input: ', key='input')
+submit = st.button('Ask Gemini')
+
+# after submit
+
+if submit:
+    response = get_gemini(input_qa)
+    st.subheader("Your Answer:")
+    st.write(response)
